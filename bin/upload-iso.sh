8ee0dfe3e365e62d06cc3083bd06f3f8e7a877db
Ellen Marie Dash <me@duckie.co>
2023-07-19 00:28:52 -0400

    Add bin/upload-iso.sh.

---
+++ bin/upload-iso.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export LIBVIRT_DEFAULT_URI="qemu+ssh://puppy@rockpro64/system"
+
+if [ -z "$1" ]; then
+        echo "Usage: $0 DEST_VOL [SRC_FILE]"
+        echo "Where:"
+        echo "  DEST_VOL  is the name of the volume being created"
+        echo "  SRC_FILE  is the name of the file being copied (default: same as DEST_VOL)"
+        exit 1
+fi
+
+DEST_VOL="$1"
+SRC_FILE="$2"
+
+[ -z "$SRC_FILE" ] && SRC_FILE="$DEST_VOL"
+
+size=$(stat -Lc%s "$SRC_FILE")
+virsh vol-create-as isos "$SRC_FILE" $size --format raw
+virsh vol-upload --pool isos "$DEST_VOL" "$SRC_FILE"
